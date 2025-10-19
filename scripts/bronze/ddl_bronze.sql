b2ddb85197838208c96adf56cedce1dfb7ba8ed2
saliscode <48163020+saliscode@users.noreply.github.com>
2025-10-19 13:33:05 -0400

    Create ddl_bronze.sql

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,14 @@
+/*
+==============================================================================
+DDL Script: Create Bronze Table
+==============================================================================
+Script Purpose:
+   This script creates tables in the 'bronze' schema, dropping existing tables
+  if they already exist.
+  Run this script to re-define the DDL structure of 'bronze' tables
+
+
+
+*/
+
+/*-----------I will paste the code here soon...........*/
