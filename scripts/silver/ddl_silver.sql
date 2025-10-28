e7341dabb07a43b70e88463612c4e4d7948713d4
saliscode <48163020+saliscode@users.noreply.github.com>
2025-10-27 21:28:11 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -19,7 +19,7 @@ CREATE TABLE silver.crm_cust_info (
   cst_key NVARCHAR(50),
   cst_firstname NVARCHAR(50),
   cst_lastname NVARCHAR(50),
-  cst_material_status NVARCHAR(50),
+  cst_marital_status NVARCHAR(50),
   cst_gndr NVARCHAR(50),
   cst_create_date DATE,
   dwh_create_date DATETIME2 DEFAULT GETDATE()
@@ -31,12 +31,13 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 GO
 CREATE TABLE silver.crm_prd_info (
   prd_id INT,
+  cat_id NVARCHAR(50),
   prd_key NVARCHAR(50),
   prd_nm NVARCHAR(50),
   prd_cost INT,
   prd_line NVARCHAR(50),
-  prd_start_dt DATETIME,
-  prd_end_dt DATETIME,
+  prd_start_dt DATE,
+  prd_end_dt DATE,
   dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -47,11 +48,12 @@ CREATE TABLE silver.crm_sales_details (
   sls_ord_num NVARCHAR(50),
   sls_prd_key NVARCHAR(50),
   sls_cust_id INT,
-  sls_order_dt INT,
-  sls_ship_dt INT,
-  sls_due_dt INT,
+  sls_order_dt DATE,
+  sls_ship_dt DATE,
+  sls_due_dt DATE,
   sls_sales INT,
   sls_quantity INT,
+  sls_price   INT,
   dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
